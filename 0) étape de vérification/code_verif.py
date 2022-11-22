ad92f6b195d104a11c806925963567bf368c93a2
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-22 10:32:11 +0100

    Update code_verif.py

--- 0) étape de vérification/code_verif.py
+++ 0) étape de vérification/code_verif.py
@@ -5,9 +5,11 @@ import os
 
 #Vérifier que ce n'est pas un répertoire mais bien un fichier
 
+file_name=sys.argv[1]
+
 if os.path.isdir(file_name) == 1:
     print("veuillez donner un fichier et non pas un répertoire")
-    exit()
+    return "F"
 
 Vérifier que l'extention est un .SAM si .bam message d'erreur demandant à l'utilisateur de mettre un .sam Tiago
 if file_name.split(".")[-1] != "sam" :
@@ -20,4 +22,4 @@ if os.path.getsize(file_name) == 0:
     print("Le fichier est vide")
     exit()
 
-
+return "ok"
