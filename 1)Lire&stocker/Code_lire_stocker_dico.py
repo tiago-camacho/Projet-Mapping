37315838ee21f73a62209d5ebe43bedd04886185
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-22 10:39:24 +0100

    Update Code_lire_stocker_dico.py

--- 1)Lire&stocker/Code_lire_stocker_dico.py
+++ 1)Lire&stocker/Code_lire_stocker_dico.py
@@ -4,7 +4,7 @@ dico = {}
 lines = file.readlines()
 
 for line in lines:
-    if line[0] != "@":
+    if line[0][0] != "@":
         temp =line.split("\t")
         if temp[0] in dico.keys():
             dico[temp[0]].append(temp[1:])
