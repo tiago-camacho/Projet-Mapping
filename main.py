ff167e2b3398fc85beee1305e3b7831c475a0cae
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-22 11:10:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+#!/usr/bin/env python3
 
+import sys, os
+file = sys.argv[1]
+verif=verif.py(file)
+if verif=="T" : 
+  
+  
+  
+read_dico.py
+
+
+
+number_of_data.py 
+
+
+
+flag_calcul.py
+
+
+number_of_mapped_read.py
+
+mapping_quality.py
+
+pair_propper_mapped.py 
+
+totally_aligned_read.py
