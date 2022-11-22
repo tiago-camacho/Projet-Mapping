31f0412d61be82782853787806e2fd8e5a59ad40
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-11-22 10:59:12 +0100

    Update verif.py

--- verif.py
+++ verif.py
@@ -1,1 +1,24 @@
 
+import sys
+import os
+
+#Vérifier que ce n'est pas un répertoire mais bien un fichier
+
+file_name=sys.argv[1]
+
+if os.path.isdir(file_name) == 1:
+    print("veuillez donner un fichier et non pas un répertoire")
+    return "F"
+
+Vérifier que l'extention est un .SAM si .bam message d'erreur demandant à l'utilisateur de mettre un .sam Tiago
+if file_name.split(".")[-1] != "sam" :
+    print("veuillez donner un fichier dont l'extension est .sam")
+    exit ()
+    
+Vérifier que ce n'est pas un fichier vide Ally
+
+if os.path.getsize(file_name) == 0:
+    print("Le fichier est vide")
+    exit()
+
+return "ok"
