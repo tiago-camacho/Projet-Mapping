4e65d1a46c906c06715848fc1ac7a1e6d0caf007
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-11-22 11:17:57 +0100

    Update read_dico.py

--- read_dico.py
+++ read_dico.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
 
+file =open(sys.argv[1])
+
+dico = {}
+lines = file.readlines()
+
+for line in lines:
+    if line[0][0] != "@":
+        temp =line.split("\t")
+        if temp[0] in dico.keys():
+            dico[temp[0]].append(temp[1:])
+        else:
+            dico[temp[0]]=[temp[1:]]
+    
+file.close()
