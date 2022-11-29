8bad0d5430139b4a50a29809de7717b7addc598a
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-11-29 07:52:30 +0100

    Update totally_aligned_read.py

--- totally_aligned_read.py
+++ totally_aligned_read.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
-test CIGAR = nombre de 1,2 ou 3 chiffre)+M veut dire que c'est totalement mappé  TIAGO !
-si != M pas bien mappé 
-[1-500]M
-[0-9][0-9][0-9]M ou [0-9][0-9]M
+#test CIGAR = nombre de 1,2 ou 3 chiffre)+M veut dire que c'est totalement mappé  TIAGO !
+#si != M pas bien mappé 
+#[1-500]M
+#[0-9][0-9][0-9]M ou [0-9][0-9]M
 
-on selectionne seulement les lignes qui vérifie cette information 
+#on selectionne seulement les lignes qui vérifie cette information 
 
 
 
-Pour i de 0 a n+1 par pas de 1
-si colone cigar = 100M alors 
-compteur +1
-i+1
-sinon 
-i+1
+#Pour i de 0 a n+1 par pas de 1
+#si colone cigar = 100M alors 
+#compteur +1
+#i+1
+#sinon 
+#i+1
+
+for key , valeur in dico 
+  if (colone5) == ([0-9][0-9][0-9]M or [0-9][0-9]M);
+    
