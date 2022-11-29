f3540602ae23868252e8163aa1318718e3c88480
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-29 10:34:53 +0100

    Update read_dico.py

--- read_dico.py
+++ read_dico.py
@@ -6,11 +6,9 @@ lines = file.readlines()
 
 for line in lines:
     if line[0][0] != "@":
-        temp =line.split("\t")
-          #pour la colone 1 de la ligne lus si modulo de son calcul de flag =  4 (unmapped) ou 8 (mate unmapped) alors on ne prend pas la ligne.
-             if flag_calcul.py() != 4 or 8 : 
-                if temp[0] in dico.keys():
-                      dico[temp[0]].append(temp[1:])
+        temp =line.split("\t") 
+           if temp[0] in dico.keys():
+               dico[temp[0]].append(temp[1:])
                   else:
                         dico[temp[0]]=[temp[1:]]
     
