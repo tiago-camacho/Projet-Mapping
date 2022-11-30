b711f5cddca3823b454b7844649dd951db91f60c
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-11-30 18:03:48 +0100

    Update number_of_mapped_read.py

--- number_of_mapped_read.py
+++ number_of_mapped_read.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
 
+for cle,valeur in dico.items() [1] :
+  if cle,valeur == 0 or * :
+    del dico cle,valeur
+    
+for cle,valeur in dico.items() [2] :
+  if cle,valeur == 0 or * :
+    del dico cle,valeur
+    
+                       
