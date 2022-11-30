73ae1503dec44152b604e073f2b465413470368e
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-11-30 18:56:30 +0100

    Update flag_calcul.py

--- flag_calcul.py
+++ flag_calcul.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-#est-ce que ces lignes de code prennent bien en compte la 1er élément de la valeur du dico
-for cle,valeur in dico.items() [0] : 
-     for valeur in dico.items() [0][]
+#est-ce que ces lignes de code prennent bien en compte la 1er élément de la valeur du dico? oui 
+for cle,valeur in dico.items() : 
+     for v in valeur [0]:
 
 a = {}
 if X-2048 >=0:
