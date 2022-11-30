d2e0b8b263e4ff4d4501acb1352c64d8f1f70514
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-30 19:33:10 +0100

    Update number_of_mapped_read.py

--- number_of_mapped_read.py
+++ number_of_mapped_read.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-for cle,valeur in dico.items() [1] :
-  if cle,valeur == 0 or * :
-    del dico cle,valeur
+#for cle,valeur in dico.items() [1] :
+ # if cle,valeur == 0 or * :
+  #  del dico cle,valeur
     
-for cle,valeur in dico.items() [2] :
-  if cle,valeur == 0 or * :
-    del dico cle,valeur
+#for cle,valeur in dico.items() [2] :
+ # if cle,valeur == 0 or * :
+  #  del dico cle,valeur
    
 for cle, valeur in dico.items() : 
   for v in valeur :
