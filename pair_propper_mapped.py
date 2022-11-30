77a5be3b1c7be3911fdfe345c2252e9ef9bc27f2
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-30 20:30:21 +0100

    Update pair_propper_mapped.py

--- pair_propper_mapped.py
+++ pair_propper_mapped.py
@@ -14,11 +14,3 @@ flag = 2, 16, 32 et si position [6]
             counter += 1
     else : 
         pair_not_proper_mapped={key,value}
-        
-        
-
-
-
- #dictionnaire 
- flag = {2048: "Supplementary alignment", 1024: "Read is PCR or optical duplicate",1: "Read paired", 2:	"Read mapped in proper pair", 4: "Read unmapped", 8: "Mate unmapped", 16: "Read reverse strand", 32: "Mate reverse strand", 64:	"First in pair", 128:	"Second in pair", 256:	"Not primary alignment", 512:	"Read fails platform/vendor quality checks" }
-flag= [a]
