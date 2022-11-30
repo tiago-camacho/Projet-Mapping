90604efdb3fabbf37fddf3c7e0011004bac94d0c
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-30 20:27:32 +0100

    Update pair_propper_mapped.py

--- pair_propper_mapped.py
+++ pair_propper_mapped.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
+
+
+counter =+0
+
 flag = 2, 16, 32 et si position [6] 
-    
+
+ f16 = 16
+ f32 = 32
+  for key,value in dico.items():
+    if int(value[0][0]) & 2==2 and int(value[1][0]) & 2==2 :
+        if int(value[0][0]) & 16==16 or int(value[1][0]) & 16==16 and int(value[0][0]) & 32==32 or int(value[1][0]) & 32==32 :
+            if ((value [0][2])-(value[1][2])<=sys.argv[2]
+            counter += 1
+    else : 
+        pair_not_proper_mapped={key,value}
+        
+        
+
+
+
  #dictionnaire 
  flag = {2048: "Supplementary alignment", 1024: "Read is PCR or optical duplicate",1: "Read paired", 2:	"Read mapped in proper pair", 4: "Read unmapped", 8: "Mate unmapped", 16: "Read reverse strand", 32: "Mate reverse strand", 64:	"First in pair", 128:	"Second in pair", 256:	"Not primary alignment", 512:	"Read fails platform/vendor quality checks" }
 flag= [a]
