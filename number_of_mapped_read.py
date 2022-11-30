ac7099fd356434526c825b8f4cdda0d1a846e9c8
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-11-30 19:11:14 +0100

    Update number_of_mapped_read.py

--- number_of_mapped_read.py
+++ number_of_mapped_read.py
@@ -7,5 +7,21 @@ for cle,valeur in dico.items() [1] :
 for cle,valeur in dico.items() [2] :
   if cle,valeur == 0 or * :
     del dico cle,valeur
-    
+   
+for cle, valeur in dico.items() : 
+  for v in valeur :
+    if v[0] & 4 != 4 : 
+      del dico cle valeur
+      
+pour savoir si deux clones sont bien mappés en face  : 
+  
+  f16 = 16
+  F32 = 32
+  for key,value in dico.items():
+    if int(value[0][0]) & 16 or int(value[1][0]) & 32 and int(value[1][0]) & 32 or int(value[0][0]) :
+      
+      
+      
+
+
                        
