dcb6fdc05e1e99cb065ea40abf83f36e68a5bdda
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-11-30 16:54:44 +0100

    Update number_of_data.py

--- number_of_data.py
+++ number_of_data.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-
+#compte toute les valeur dans le dico
 number_total_of_data_counter = 0
 for cle, valeur in dico.items(): 
 number_total_of_data_counter += len(valeur)
 print("number_total_of_data_counter ="+number_total_of_data_counter)
 
-
+#compte toute les paire dans le dico
 number_of_paired_reads_data_counter = 0
 for cle, valeur in dico.items(): 
    if cle, valeur in dico.item() (!= 2) :
