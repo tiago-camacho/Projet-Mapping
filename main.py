f33ec5cb7c029985e2a1d1ca38e57028762f131e
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-12-01 13:22:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
 import sys, os
-file = sys.argv[1]
-verif=verif.py(file)
-if verif !="T" : 
+
+if os.path.isdir(sys.argv[1]) == 1:
+    print("veuillez donner un fichier et non pas un répertoire")
+    return "F"
+
+#Vérifier que l'extention est un .SAM si .bam message d'erreur demandant à l'utilisateur de mettre un .sam 
+
+if (sys.argv[1]).split(".")[-1] != "sam" :
+    print("veuillez donner un fichier dont l'extension est .sam")
+    return "F"
+    
+#Vérifier que ce n'est pas un fichier vide 
+
+if os.path.getsize(sys.argv[1]) == 0:
+    print("Le fichier est vide")
+    return "F"
+return "T"
   exit()
 else : 
   print("fichier ok")
