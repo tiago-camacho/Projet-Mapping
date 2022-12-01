eeef8bf505f391f0f32e8cd552df75d77fd66051
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-12-01 13:34:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 
 import sys, os
-
+###########################################################  verification of your file  #############################################################################
 if os.path.isdir(sys.argv[1]) == 1:
     print("veuillez donner un fichier et non pas un répertoire")
     return "F"
 
-#Vérifier que l'extention est un .SAM si .bam message d'erreur demandant à l'utilisateur de mettre un .sam 
-
 if (sys.argv[1]).split(".")[-1] != "sam" :
     print("veuillez donner un fichier dont l'extension est .sam")
     return "F"
-    
-#Vérifier que ce n'est pas un fichier vide 
 
 if os.path.getsize(sys.argv[1]) == 0:
     print("Le fichier est vide")
@@ -22,7 +18,7 @@ return "T"
 else : 
   print("fichier ok")
   
-## create a dictionnary of your file.sam
+##################################################### create a dictionnary of your file.sam  ########################################################################
 dico = {}
 file =open(sys.argv[1])
 lines = file.readlines()
@@ -37,7 +33,7 @@ for line in lines:
     
 file.close()
 
-## counting your starting data number
+################################################################## counting your starting data number  ##############################################################
 
 #compte toute les valeur dans le dico
 number_total_of_data_counter = 0
@@ -54,7 +50,7 @@ for cle, valeur in dico.items():
  number_of_paired_reads_data_counter += len(valeur)
 print("number_of_paired_reads_data_counter =" number_of_paired_reads_data_counter)
 
-## counting your number of mapped read
+######################################################### counting your number of mapped read  #####################################################################
 for cle, valeur in dico.items() : 
   for v in valeur :
     if v[0] & 4 != 4 : 
@@ -68,11 +64,11 @@ pour savoir si deux clones sont bien mappés en face  :
     if int(value[0][0]) & 16 or int(value[1][0]) & 32 and int(value[1][0]) & 32 or int(value[0][0]) :
         
         
-## mapping quality 
+################################################################## mapping quality #################################################################################
 si la colonne 3 de notre value dans le dico > ou = à sys.argv[3]
 compter faire un dico et supprimer dans le grand dico
 
-# number of pair proper mapped
+################################################################### number of pair proper mapped  ###################################################################
 
 pair_not_proper_mapped={}
 counter =+0
@@ -90,4 +86,32 @@ flag = 2, 16, 32 et si position [6]
      pair_not_proper_mapped[key] = value
      del dico[key]
 
-totaly_aligned_read.py
+#####################################################################  totaly aligned read  ########################################################################
+
+#test CIGAR = nombre de 1,2 ou 3 chiffre)+M veut dire que c'est totalement mappé  TIAGO !
+#si != M pas bien mappé 
+#[1-500]M
+#[0-9][0-9][0-9]M ou [0-9][0-9]M
+
+#on selectionne seulement les lignes qui vérifie cette information 
+
+
+
+#Pour i de 0 a n+1 par pas de 1
+#si colone cigar = 100M alors 
+#compteur +1
+#i+1
+#sinon 
+#i+1
+for key,value in dico.items():
+  a finir 
+  
+  
+  
+  
+  
+totally_aligned = [];
+valeur = dico.values()
+for  valeur in dico 
+  if (colone5) != ([0-9][0-9][0-9]M or [0-9][0-9]M);
+     del dico[key]
