fa918de0468153dce954b8492043aad14a1f5136
allysonFDSmtp <117992265+allysonFDSmtp@users.noreply.github.com>
2022-12-01 13:25:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,22 @@ for line in lines:
     
 file.close()
 
-number_of_data.py 
+## counting your starting data number
+
+#compte toute les valeur dans le dico
+number_total_of_data_counter = 0
+for cle, valeur in dico.items(): 
+number_total_of_data_counter += len(valeur)
+print("number_total_of_data_counter ="+number_total_of_data_counter)
+
+#compte toute les paire dans le dico
+number_of_paired_reads_data_counter = 0
+for cle, valeur in dico.items(): 
+   if cle, valeur in dico.item() (!= 2) :
+    number_of_paired_reads_data_counter += 0
+    else : 
+ number_of_paired_reads_data_counter += len(valeur)
+print("number_of_paired_reads_data_counter =" number_of_paired_reads_data_counter)
 
 flag_calcul.py
 
