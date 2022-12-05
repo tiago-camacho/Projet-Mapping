08d080ba570272bf253ec589ca969ee2ba6a180d
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-12-05 19:27:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ for key,value in dico.items():
         if (int(value[0][0]) & 16==16 and int(value[1][0]) & 32==32) or (int(value[0][0]) & 32==32 and int(value[1][0]) & 16==16) :#if they are mated
             if abs((value [0][2])-(value[1][2]))<=sys.argv[2]: #and if the distance is < or = your exigence
                pair_poper_mapped_counter += 1
-                continue 
+            continue 
      else :
         pair_not_proper_mapped[key] = value #the read is stored in an other dictionnary in case you need it
         del dico[key] #the key is erased from the starting dictionnary 
