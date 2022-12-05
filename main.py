802666afd9c32ca568f055d79e26d5fad2ff61bd
Tiago Camacho <114236802+tiago-camacho@users.noreply.github.com>
2022-12-05 17:23:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ lines = file.readlines()
 for line in lines:
     if line[0][0] != "@":
         temp =line.split("\t") 
-           if temp[0] in dico.keys():
-               dico[temp[0]].append(temp[1:])
-                  else:
-                        dico[temp[0]]=[temp[1:]]
+        if temp[0] in dico.keys():
+            dico[temp[0]].append(temp[1:])
+        else:
+            dico[temp[0]]=[temp[1:]]
     
 file.close()
 
